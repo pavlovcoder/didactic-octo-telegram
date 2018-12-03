07521bc3d96d9d23c9b5ed025128a44c1d35a517
unknown <w.pavlov@hit-kody.com.pl>
2018-12-03 23:51:53 +0100

    Commit #1

---
+++ exercise_2.py
@@ -0,0 +1,30 @@
+print(
+  '-----------------------------------------\n'\
+  'Practical python education || Exercise-2:\n'\
+  '-----------------------------------------\n'
+  )
+
+print(
+  'Task:\n'\
+  '-----------------------------------------\n'\
+  'Write a Python program to get the Python version you are using."\n'
+  )
+
+print(
+  'Solution:\n'\
+  '-----------------------------------------'\
+  )
+
+import sys
+print(
+  'Python version:\n'\
+  f'{sys.version}\n'\
+  'Version info.\n'\
+  f'{sys.version_info}'
+)
+
+print(
+  '\n-----------------------------------------\n'\
+  'Copyright 2018 Vladimir Pavlov. All Rights Reserved.\n'\
+  '-----------------------------------------'
+  )
